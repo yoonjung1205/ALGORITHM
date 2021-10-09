1bc9d2ce5b6599d6baf93a40dc9038d924795998
정윤정 <yoonjung1205@naver.com>
2021-10-09 23:54:00 +0900

    211009 || upload programmers

--- PROGRAMMERS/2018_kakao/방금그곡.py
+++ PROGRAMMERS/2018_kakao/방금그곡.py
@@ -6,6 +6,7 @@ def solution(m, musicinfos):
         m = m.replace('D#','d')
         m = m.replace('F#','f')
         m = m.replace('G#','g')
+        m = m.replace('E#','e')
     result = []
     for musicinfo in musicinfos:
         music_s,music_e,title,info = musicinfo.split(',')
@@ -24,14 +25,16 @@ def solution(m, musicinfos):
             info = info.replace('D#','d')
             info = info.replace('F#','f')
             info = info.replace('G#','g')
+            info = info.replace('E#','e')
         print(music_s,music_e,title,info)
         
         # 음악길이
         m_l = len(info)
         
-        # 재생시간 동안 재생된 음
+        # 재생시간 동안 재생된 음(= 악보정보*몫 + 나머지), 나머지가 아니어도 된다.. 
         music = info * (playing_time//m_l) + info[:playing_time]
         print(m,music)
+        
         
         # 입력된 음악이 재생된 음악에 있는 경우
         if m in music:
@@ -46,7 +49,8 @@ def solution(m, musicinfos):
         
     # 일치하는 음악이 여러개인 경우
     else:
-        result = result.sorted(key=lambda x:(-x[0],x[1]))   # 재생시간이 가장길고(내림차순), 먼저 들어온 순(오름차순)으로 정렬
+        result = sorted(result,key=lambda x:(-x[0],x[1]))   # 재생시간이 가장길고(내림차순), 먼저 들어온 순(오름차순)으로 정렬
+        #result.sort(key=lambda x:(-x[0],x[1]))
         answer = result[0][2]
     
     return answer
