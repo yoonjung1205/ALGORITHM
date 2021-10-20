4aa5df4aede6a8b164f29f2e8aaacca2586a93b7
정윤정 <yoonjung1205@naver.com>
2021-10-20 19:46:09 +0900

    211019 || upload 이분탐색

--- BOJ/이분탐색/2776.py
+++ BOJ/이분탐색/2776.py
@@ -1,1 +1,29 @@
+def search(arr, key):
+    l = 0
+    h = len(arr) - 1
+
+    while l <= h:
+        mid = (l + h) // 2
+        if arr[mid] == key:
+            return 1
+        elif arr[mid] < key:
+            l = mid + 1
+        else:
+            h = mid - 1
+
+    return 0
+
+
 T = int(input())
+
+
+for tc in range(T):
+    N = int(input())
+    note1 = list(map(int, input().split()))
+    M = int(input())
+    note2 = list(map(int, input().split()))
+    note1.sort()
+
+    for num in note2:
+        print(search(note1,num))
+
