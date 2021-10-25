0af8ed4f07d89bbe0940e52ae8de9f82b2038efb
정윤정 <yoonjung1205@naver.com>
2021-10-26 00:48:55 +0900

    211026 || upload programmers

--- PROGRAMMERS/2019_kakao/오픈채팅방.py
+++ PROGRAMMERS/2019_kakao/오픈채팅방.py
@@ -2,17 +2,29 @@ def solution(record):
     answer = []
     name_result = {}
     status_result = []
-    total_status = ''
+
     for info in record:
         if 'Leave' in info:
             status,user_id = info.split()
-        else: status, user_id, name = info.split()
-        status_result.append([status,user_id])
-        name_result[user_id] = name
+            status_result.append([status,user_id])
+        else:
+            status, user_id, name = info.split()
+            status_result.append([status,user_id])
+            name_result[user_id] = name
+            
     print(status_result)
     print(name_result)
+
+    for i in range(len(status_result)):
+        if status_result[i][0] =="Enter":
+            answer.append(name_result[status_result[i][1]] + "님이 들어왔습니다.")
+        elif status_result[i][0] =="Leave":
+            answer.append(name_result[status_result[i][1]] + "님이 나갔습니다.")
     return answer
 
 
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
-solution(record)
+# record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+# record = ["Enter uid1234 Muzi","Leave uid1234","Enter abc1234 Prodo","Change abc1234 Apeach","Enter uid1234 Ryan","Enter Ryan Ryan"]
+record = ["Enter uid0606 Gimoi", "Enter uid4567 Prodo", "Leave uid0606", "Enter uid1234 Prodo", "Change uid1234 OhYeah"]
+
+print(solution(record))
