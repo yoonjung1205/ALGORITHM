6a7be77cf6e93ed94d5089913710c59f0972ad2f
정윤정 <yoonjung1205@naver.com>
2021-11-10 23:26:30 +0900

    211110 || upload BOJ

---
+++ BOJ/UF,Dijkstra/1261_알고스팟.py
@@ -0,0 +1,36 @@
+import heapq
+
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+
+def dijkstra(r,c):
+    global ans
+    q = []
+    heapq.heappush(q,[0,r,c])
+    d[0][0] = 0
+    while q:
+        dist,x,y = heapq.heappop(q)
+        if x == N-1 and y == M-1:
+            return
+
+        if dist > d[x][y]:
+            continue
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                n_dist = dist + miro[nx][ny]
+                if n_dist < d[nx][ny]:
+                    d[nx][ny] = n_dist
+                    heapq.heappush(q,[n_dist,nx,ny])
+
+# M: 가로크기, N: 세로크기
+M, N = map(int,input().split())
+miro = [list(map(int,input())) for _ in range(N)]
+# print(miro)
+INF = 987654321
+d = [[INF] * M for _ in range(N)]
+# print(d)
+ans = 0
+dijkstra(0,0)
+print(d[N-1][M-1])
