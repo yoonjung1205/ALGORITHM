c7c89e4cbf01e50a7c8bb9f51f780dc6ee38b6b0
정윤정 <yoonjung1205@naver.com>
2022-01-19 23:52:40 +0900

    220119 || upload PROGRAMMERS

--- PROGRAMMERS/2021DevMatching웹백엔드_행렬테두리회전하기.py
+++ PROGRAMMERS/2021DevMatching웹백엔드_행렬테두리회전하기.py
@@ -1,5 +1,16 @@
 def solution(rows, columns, queries):
     answer = []
+    matrix = [[0]*(columns+1) for _ in range(rows+1)]
+    print(matrix)
+
+    for i in range(1,rows+1):
+        for j in range(1,columns+1):
+            matrix[i][j] = ((i-1)*columns + j)
+
+    for num in range(len(queries)):
+        matrix[queries[num]][]
+
+    print(matrix)
     return answer
 
 
