14b13b807104f86b4034375de880ac0b86f6cd2f
정윤정 <yoonjung1205@naver.com>
2022-01-20 22:49:36 +0900

    220120 || upload PROGRAMMERS

--- PROGRAMMERS/2020카카오인턴_수식최대화.py
+++ PROGRAMMERS/2020카카오인턴_수식최대화.py
@@ -1,3 +1,49 @@
+from itertools import permutations
+
+
+def op_cal(num1, num2, ope):
+    if ope == '*':
+        return num1 * num2
+    if ope == '+':
+        return num1 + num2
+    if ope == '-':
+        return num1 - num2
+
+
 def solution(expression):
     answer = 0
+    ope = ['*', '+', '-']
+    ope = list(permutations(ope, 3))
+    # print(ope)
+
+    num = list(expression)
+    arr = []
+    tmp = ''
+    # print(list(num))
+    for w in range(len(num)):
+        if num[w] not in '*-+':
+            tmp += num[w]
+        else:
+            arr.append(tmp)
+            arr.append(num[w])
+            tmp = ''
+        if w == len(num) - 1:
+            arr.append(tmp)
+
+    result = []
+    for op in ope:
+        arr2 = arr[:]
+        for o in op:
+            s = []
+            while arr2:
+                p = arr2.pop(0)
+                if p != o:
+                    s.append(p)
+                else:
+                    s.append(op_cal(int(s.pop()), int(arr2.pop(0)), o))
+            arr2 = s
+        result.append(abs(int(s[0])))
+
+    answer = max(result)
+
     return answer
--- PROGRAMMERS/2021DevMatching웹백엔드_행렬테두리회전하기.py
+++ PROGRAMMERS/2021DevMatching웹백엔드_행렬테두리회전하기.py
@@ -1,14 +1,27 @@
 def solution(rows, columns, queries):
     answer = []
     matrix = [[0]*(columns+1) for _ in range(rows+1)]
+
     print(matrix)
 
     for i in range(1,rows+1):
         for j in range(1,columns+1):
             matrix[i][j] = ((i-1)*columns + j)
 
-    for num in range(len(queries)):
-        matrix[queries[num]][]
+
+    for query in queries:
+        r1 = query[0]
+        c1 = query[1]
+        r2 = query[2]
+        c2 = query[3]
+        p = matrix[r1][c2]
+        for i in range(c2,c1,-1):
+            tmp = matrix[r1][i-1]
+            matrix[r1][i] = tmp
+        for i in range(r2,r1+1,-1):
+            tmp = matrix[i-1][c2]
+            matrix[i][c2] = tmp
+        matrix[r1+1][c2] = p
 
     print(matrix)
     return answer
