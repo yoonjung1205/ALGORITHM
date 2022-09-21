840f74a7fda3d270e920851b2efc034c656ffd3e
정윤정 <yoonjung1205@naver.com>
2022-09-22 00:02:54 +0900

    220922 || upload BOJ

---
+++ BOJ/여러주제들/17609_회문.py
@@ -0,0 +1,37 @@
+T = int(input())
+
+def check_2(string, start, end):
+    while start < end:
+        if string[start] == string[end]:
+            start += 1
+            end -= 1
+        else:
+            return False
+
+    return True
+
+
+def check(string):
+    start = 0
+    end = len(string) - 1
+
+    while start < end:
+        if string[start] == string[end]:
+            start += 1
+            end -= 1
+        else:
+            if check_2(string,start+1,end):
+                return 1
+
+            if check_2(string,start,end-1):
+                return 1
+
+            else:
+                return 2
+    return 0
+
+
+for _ in range(T):
+    string = input()
+    result = check(string)
+    print(result)
