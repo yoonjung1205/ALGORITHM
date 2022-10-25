d7ab6fd2b55dedeab9a9d6319a881ce2f9ef4959
정윤정 <yoonjung1205@naver.com>
2022-10-25 22:04:31 +0900

    221025 || upload BOJ

---
+++ BOJ/bfs_dfs/5014_스타트링크.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+F,S,G,U,D = map(int,input().split())
+result = "use the stairs"
+visit = [0] * (F+1)
+def bfs(start, count):
+    global result, now
+    q = deque()
+    q.append([start,count])
+
+    while q:
+        now, cnt = q.popleft()
+        nu = now + U
+        nd = now - D
+        if now == G:
+            result = cnt
+            return
+        elif not visit[now] and (1 <= nu <= F or 1 <= nd <= F):
+            visit[now] = 1
+            if 0 <= nu <= F:
+                q.append([nu,cnt + 1])
+            if 0 <= nd <= F:
+                q.append([nd,cnt + 1])
+
+bfs(S,0)
+
+
+print(result)
